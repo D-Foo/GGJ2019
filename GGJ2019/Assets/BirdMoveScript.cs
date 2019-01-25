d11c3be5d6667f0ddec3c18487dc2cede2adaea5
D-Foo <davidfoo98@gmail.com>
2019-01-25 22:25:02 +0000

    Camera now rotates around cylinder with cube and looks at cylinder

---
+++ GGJ2019/Assets/BirdMoveScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMoveScript : MonoBehaviour {
+
+    private float cylinderX;
+    private float cylinderZ;
+    static private float rotAngle;
+    
+	// Use this for initialization
+	void Start () {
+        cylinderX = GameObject.FindGameObjectWithTag("Center Cylinder").transform.position.x;
+        cylinderX = GameObject.FindGameObjectWithTag("Center Cylinder").transform.position.z;
+#if UNITY_EDITOR
+     Debug.Log("XZ (" + cylinderX + ", " + cylinderZ + ")");
+#endif
+        rotAngle = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotAngle -= 30.0f * Time.deltaTime;
+#if UNITY_EDITOR
+            Debug.Log("LEFT");
+#endif
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotAngle += 30.0f * Time.deltaTime;
+#if UNITY_EDITOR
+            Debug.Log("RIGHT");
+#endif
+        }
+
+        gameObject.transform.position = new Vector3(5 * Mathf.Sin(Mathf.Deg2Rad * rotAngle), gameObject.transform.position.y, 5* -Mathf.Cos(Mathf.Deg2Rad * rotAngle));
+#if UNITY_EDITOR
+        Debug.Log("(" + Mathf.Sin(Mathf.Deg2Rad * rotAngle) + ", " + gameObject.transform.position.y + ", " + -Mathf.Cos(Mathf.Deg2Rad * rotAngle) +  ")");
+#endif
+    }
+
+    static public float GetRotAngle()
+    {
+        return rotAngle;
+    }
+}
---
+++ GGJ2019/Assets/CameraMoveScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveScript : MonoBehaviour {
+
+    private float cylinderX;
+    private float cylinderZ;
+
+    // Use this for initialization
+    void Start () {
+        cylinderX = GameObject.FindGameObjectWithTag("Center Cylinder").transform.position.x;
+        cylinderX = GameObject.FindGameObjectWithTag("Center Cylinder").transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //float playerRot = GameObject.FindGameObjectWithTag("Player").GetComponent("rotAngle");
+        float playerRot = BirdMoveScript.GetRotAngle();
+        //Camera thisCam = gameObject.GetComponent<Camera>();
+        //gameObject.transform.rotation = Quaternion.Euler(-playerRot, gameObject.transform.rotation.y, gameObject.transform.rotation.z);
+        gameObject.transform.LookAt(GameObject.FindGameObjectWithTag("Center Cylinder").transform, new Vector3(0, 1, 0));
+        gameObject.transform.position = new Vector3(10 * Mathf.Sin(Mathf.Deg2Rad * playerRot), gameObject.transform.position.y, 10 * -Mathf.Cos(Mathf.Deg2Rad * playerRot));
+    }
+}
