92d80e2178601197c5b02e397cddf304a5bc8e0f
GGeorge97 <gavin-george@live.co.uk>
2019-01-27 14:44:11 +0000

    Altered collisions for camera

--- GGJ2019/Assets/BirdCollisionScript.cs
+++ GGJ2019/Assets/BirdCollisionScript.cs
@@ -19,11 +19,15 @@ public class BirdCollisionScript : MonoBehaviour
             case "Branch":
                 viewAdjust = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 4, gameObject.transform.position.z);
                 mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, viewAdjust, 4.0f * Time.deltaTime);
+
+                break;
+
+            case "Floor":
+                viewAdjust = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 3, gameObject.transform.position.z);
+                mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, viewAdjust, 2.0f * Time.deltaTime);
                 break;
 
             default:
-                viewAdjust = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 3, gameObject.transform.position.z);
-                mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, viewAdjust, 2.0f * Time.deltaTime);
                 break;
         }
     }
